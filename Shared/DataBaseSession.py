9bf1ab90160ef66c641b65c25daf3842f92805cb
Timur Nikolaev <timplay42@mail.ru>
2026-01-21 12:17:20 +0300

    2 alembic migrations

--- Shared/DataBaseSession.py
+++ Shared/DataBaseSession.py
@@ -1,26 +1,27 @@
-import dotenv
+#import dotenv
 import sqlalchemy.engine.url as SQURL
-from pathlib import Path
+#from pathlib import Path
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
-current_dir = Path(__file__).parent.parent
-env_path = current_dir / '.env'
+#current_dir = Path(__file__).parent.parent
+#env_path = current_dir / '.env'
 
-config_app = dotenv.dotenv_values(env_path)
+#config_app = dotenv.dotenv_values(env_path)
 
 
 class SyncDatabaseSessions:
 
     def __init__(self):
+        # Захардкоженные настройки
         self.URL = SQURL.URL.create(
             drivername="postgresql+psycopg2",
-            username=config_app["DB_USER"],
-            password=config_app["DB_PASSWORD"],
-            host=config_app["DB_HOST"],
-            port=config_app["DB_PORT"],
-            database=config_app["DB_NAME"],
+            username="postgres",          # DB_USER
+            password="postgres",          # DB_PASSWORD
+            host="localhost",             # DB_HOST
+            port=5432,                    # DB_PORT
+            database="postgres",          # DB_NAME
         )
 
         self.engine = create_engine(
